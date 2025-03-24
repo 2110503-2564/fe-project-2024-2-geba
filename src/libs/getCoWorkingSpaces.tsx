3dff86154b6ab05bcaf5460972b027a70866b0a8
Miiizzzzz <thanasorn3449@gmail.com>
2025-03-24 21:34:07 +0700

    CoWorkingSpaceList WooHoo

--- src/libs/getCoWorkingSpaces.tsx
+++ src/libs/getCoWorkingSpaces.tsx
@@ -3,6 +3,7 @@ export default async function getCoWorkingSpaces(token:string) {
     await new Promise((resolve) => setTimeout(resolve, 300))
 
     const response = await fetch("https://cws-backend-five.vercel.app/api/v1/coWorkingSpaces", {
+        method: "GET",
         headers: {
             authorization: `Bearer ${token}`
         }
