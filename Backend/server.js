9759eb3ddae3f0a16cae101735598fa23eeca1a7
Miiizzzzz <thanasorn3449@gmail.com>
2025-03-24 12:50:09 +0700

    vercel.json added

--- Backend/server.js
+++ Backend/server.js
@@ -51,7 +51,7 @@ app.use(cors());
 
   const PORT = process.env.PORT || 5000;
 
-  const server = app.listen(PORT,console.log('Server running in', process.env.NODE_ENV, 'mode on '+ process.env.HOST + ":" + PORT));
+  const server = app.listen(PORT,console.log('Server running in', process.env.NODE_ENV, 'on '+ process.env.HOST + ":" + PORT));
 
   process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error: ${err.message}`);
